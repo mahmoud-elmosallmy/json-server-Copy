982452211266a62cb21a83fa602cd48e252b1ca6
Mahmoud <Mosallmym@gmail.com>
2023-09-15 03:01:32 +0300

    Custom Route End

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 4000
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router("routes.json")
+// const middlewares = jsonServer.defaults()
+// const port = process.env.PORT || 4000
 
-server.use(middlewares)
+// const path = require('path')
+// // const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
+// server.use(middlewares)
+// server.use(router)
+// server.listen(port, () => {
+//   console.log('JSON Server is running on ' + port)
+// })
+const server = jsonServer.create();
+const router = jsonServer.router('db.json')
 server.use(router)
-server.listen(port, () => {
-  console.log('JSON Server is running on ' + port)
+
+// This part is what I am looking for
+const customRouter = jsonServer.customRouter('routes.json')
+server.use(customRouter)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
 })
