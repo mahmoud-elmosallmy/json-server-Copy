753de67768097afd3cd5d99675bb91698567cd25
Mahmoud <Mosallmym@gmail.com>
2023-09-15 03:27:45 +0300

    Custom Route End v2

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-// const jsonServer = require('json-server')
+const jsonServer = require('json-server')
 // const server = jsonServer.create()
 // const router = jsonServer.router("routes.json")
-// const middlewares = jsonServer.defaults()
-// const port = process.env.PORT || 4000
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 4000
 
 // const path = require('path')
 // // const router = jsonServer.router(path.join(__dirname, 'db.json'))
 
-// server.use(middlewares)
+server.use(middlewares)
 // server.use(router)
 // server.listen(port, () => {
 //   console.log('JSON Server is running on ' + port)
 // })
+// ==========================================
 const server = jsonServer.create();
 const router = jsonServer.router('db.json')
 server.use(router)
@@ -20,6 +21,6 @@ server.use(router)
 const customRouter = jsonServer.customRouter('routes.json')
 server.use(customRouter)
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
